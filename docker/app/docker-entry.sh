2823371ab2401b54e115d67b59ac852e90e40a0e
ggLee <aucun6352@gmail.com>
2022-09-07 19:09:32 +0900

    수정

--- docker/app/docker-entry.sh
+++ docker/app/docker-entry.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-cargo build
+MYSQL_HOST="db"
+MYSQL_PORT=3306
+
+if [ "$ROCKET_ENV" = "development" ] || [ "$ROCKET_ENV" = "test" ]; then
+  until nc -z $MYSQL_HOST $MYSQL_PORT; do
+    echo "MySQL is not ready, sleeping..."
+    sleep 3
+  done
+  echo "Connected to Mysql!"
+fi
+
 exec "$@"
